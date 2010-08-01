8f9de60558064db44b4c561e1823e2342d042040
Grace Li <grace@gpc-f102n084.(none)>
2010-08-01 15:52:22 -0400

    updated

--- main.py
+++ main.py
@@ -1,40 +1,58 @@
+from loader import *
+import xtc
+import sys
+import glob
+import subprocess
+import shlex
 
-#replica object to encapsulate a single xtc analysis
-
-	
 def main():
 	"""docstring for main"""
+
+	current = os.getcwd()	
+	target = '/dev/shm'
+	aloader = Loader(current, target)
 	
-	#path on disk
-	#use /dev/shm
+	#list tar filse
+	tarfileslist = glob.glob("*.tar") #glob.glob(os.path.join(current, '*.tar'))
+	assert len(tarfileslist) > 0, "there are no tar files in this directory"
 	
+	print "these are the tar files to be analyzed", tarfileslist
+
+	#copy tar file into /dev/shm and attempt to inflate tar in /dev/shm
+	fnull = open(os.devnull, 'w')
 	
-	#initialize loader object
-	aloader = Loader()
-	
-	# detect all tar files on file system
-	# If no STDR_running.tar files found, quit
-	
-	#copy tar file into /dev/shm
-	#attempt to inflate tar in /dev/shm
-	
-	# analyze xtc files
-	for xtcfile in xtcList:
-		#get basename
-		#pass in path/to/file/ and basename
-		xtc = Xtc(path, xtcfile, tprfile)
+	for tarfile in tarfileslist:
+		print "processing", tarfile
+		command = "tar xvf %(tarfile)s" % vars() 
+		args = shlex.split(command)
+		code = subprocess.call(args)
+		assert code == 0, "something bad happened during untarring. Stopping."
 		
-		rgpath = xtc.rg()
-		aloader.load('rg', rowtypes.RGTable, 4, rgpath)
+		path = os.path.join(current, 'STDR_running')
+
+		# look for xtc/ dir and edr dir
+		command = "ls -l STDR_running/xtcs STDR_running/edr"
+		code = subprocess.call(shlex.split(command), stdout=fnull, stderr=fnull)
+		assert code == 0, "xtc or edr not found here"
+		
+		xtcpath = os.path.join(path, 'xtcs')
+		xtcList = os.listdir(xtcpath)
+		print xtcList
+		# analyze xtc files
+		for xtcfile in xtcList:
+			traj = xtc.Xtc(path, xtcfile, 'sh3.tpr')
+			rgpath = traj.rg()
+			#aloader.load('rg', rowtypes.RGTable, 4)
 	
-		sasapath = xtc.sasa()
-		aloader.load('sas', rowtypes.SASTable, 4, sasapath)
+			#sasapath = xtc.sasa()
+			#aloader.load('sas', rowtypes.SASTable, 4)
 		
-		#aloader.load('sas', rowtypes.SASTable, replicaMeta)
-		#aloader.load('eed', rowtypes.EEDTable, replicaMeta)
-		#aloader.load('dihedral', rowtypes.DihedralTable, replicaMeta)
-		#aloader.load('energy', rowtypes.EnergyTable, replicaMeta)
+			#aloader.load('sas', rowtypes.SASTable, replicaMeta)
+			#aloader.load('eed', rowtypes.EEDTable, replicaMeta)
+			#aloader.load('dihedral', rowtypes.DihedralTable, replicaMeta)
+			#aloader.load('energy', rowtypes.EnergyTable, replicaMeta)
 		
+	
 	
 
 if __name__ == '__main__':
--- xtc.py
+++ xtc.py
@@ -1,39 +1,61 @@
+import os
+import subprocess
+import shlex
+
 class Xtc(object):
 	"""docstring for XTC"""
 	def __init__(self, path, xtcfile, tprfile):		
 		prefix = 'ST'
 		
-		basename = xtcfile[0:len(filename)-4]
-		noprebasename = xtcfile[len(prefix):len(filename)-4]
-		self.replicanum, self.seqnum, self.temp = noprebasename.split(delimiter)
+		basename = xtcfile[0:len(xtcfile)-4]
+		self.basename = basename
+		noprebasename = xtcfile[len(prefix):len(xtcfile)-4]
+		self.replicanum, self.seqnum, self.temp = noprebasename.split('.')
 		self.path = path
 		
 		edr = basename + '.edr'
 		xtc = basename + '.xtc'
 				
 		self.tprname = os.path.join(path, tprfile)
-		self.xtcname = os.path.join(os.path.join(path, 'xtc'), xtc)
+		self.xtcname = os.path.join(os.path.join(path, 'xtcs'), xtc)
 		self.edrname = os.path.join(os.path.join(path, 'edr'), edr) 
-		
+
+		print "initialized for analysis"
+		print self.tprname, self.xtcname, self.edrname, self.path		
 		#mkdir basename
 		
-	def rg(**selection):
+	def rg(self):
 		#current dir/
 		#    rg.ndx
 		#    sasa.ndx
 		#    xtc/
 		#    edr/
 		#    system.tpr
-		
+		output = os.path.join(self.path, 'rg')
+		if not os.path.exists(output):
+			os.mkdir(os.path.join(self.path, 'rg'))		
+
+		print "outputting in", output
+		print output, "created"
+	
 		indexfile = os.path.join(self.path, 'rg.ndx')
-		selection = {'group1':'Protein'}
-		path = os.path.join(path, 'rg')
-		
-		#better to do with the subprocess module?
-		command = "echo %(selection['group1']) | g_gyrate -f %(xtcname) -s %(tprname) -n %(indexfile) -o %(rgpath)/%(basename)_rg"
-		os.system( command % vars())
+		selection = {'group1' : 'Protein'}
+		rgpath = os.path.join(self.path, 'rg')
+
+		print "checking in", self.path
+		print "checking for", indexfile
+		print "checking for", selection['group1']
+	
+		group = selection['group1']
+			
+		command = "g_gyrate -f %s -s %s -n %s -o %s" % (self.xtcname,self.tprname,indexfile,os.path.join(rgpath,self.basename))
+
+		print "running ", command
+		code = subprocess.Popen(shlex.split(command), stdin=subprocess.PIPE).communicate(selection['group1'])
+
+		#print code	
 				
-		return path
+		return rgpath
 		
 	def sasa(**selection):
 		indexfile = os.path.join(self.path, 'sasa.ndx')
