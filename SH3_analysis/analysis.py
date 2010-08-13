da0b3b5b9d81d99c012520a767011c2a4be57438
Grace Li <grace@abel.(none)>
2010-08-13 16:50:54 -0400

    update

--- SH3_analysis/analysis.py
+++ SH3_analysis/analysis.py
@@ -1,15 +1,46 @@
 # -*- coding: utf-8 -*-                                                                                                                         
+#!/usr/bin/env python
 import tables 
 import numpy
 import glob
 import sys
 import os
 import matplotlib.pylab as plt
+import ConfigParser
+import os
+
+def parseConfig():
+	#look for a configuration file in current directory
+	#if not found then looking of ~/.analysisrc
+	#if not found then quit with error
+	#if no config in current dir then plot with default params
+	config = ConfigParser.ConfigParser()
+	default = '~/config.ini'
+	current = os.path.join(os.getcwd(), 'config.ini')
+	config.readfp(open(os.path.expanduser(default)))
+	config.read(current)
+
+	if not os.path.exists(current):
+		print "you did not have a ini file in the current directory,",
+	default, "will be used to set the entire analysis settings"
+
+	return config
+
+def plot_temp(plt, temp, na):
+	avgIndex = 0
+	stdIndex = 1
+	y = na[:,avgIndex]
+	err = na[:,stdIndex]
+	plt.title(temp)
+	plt.xlabel('sampling blocks')
+	plt.errorbar(range(1,len(y)+1), y, yerr=err)
+
 
 if len(sys.argv) < 3:
 	print "usage: analysis.py path propertyname"
 	sys.exit(0)
 
+config = parseConfig()
 path = sys.argv[1]
 group, tablename, column = path.split('/')[1:]
 print group, tablename, column, path
@@ -19,14 +50,15 @@ filelist = glob.glob("*.h5")
 
 assert len(filelist) > 0
 
-Tlist = numpy.genfromtxt('templist.txt')
+Tlist = config.get('system-specific', 'temps').split()
+#numpy.genfromtxt('templist.txt')
+print Tlist
 
 count = 0
-
 start = 1
 end = 41
+
 property = {}
-#for analysisfile in filelist:
 for i in range(start, end):
 	analysisfile = "analysis_part"+str(i)+".h5"
 
@@ -38,61 +70,37 @@ for i in range(start, end):
 	file = tables.openFile(analysisfile)
 	table = file.getNode('/'+group+'/'+tablename)
 	data = []
-	for T in Tlist:
+	for Tstr in Tlist:
+		T = int(Tstr)
 		data = table.readWhere('temp==%(T)d' % vars())[column]
 		if T not in property:
 			property[T] = [[numpy.average(data), numpy.std(data)]]
 		else:
 			property[T].append([numpy.average(data), numpy.std(data)])
 
-	#count+=1
-
-	#numpy.savetxt('%(pname)s_%(count)d.txt' % vars(), property, fmt='%d %f %f')
-
-#print property
-
-avgIndex = 0
-stdIndex = 1
+	count+=1
 
 na270 = numpy.array(property[270])
-#savetxt('na.txt', na, fmt='%0.1f %f')
-
-plt.savetxt('na270.txt', na270, fmt='%0.2f %f')
-
 na300 = numpy.array(property[300])
-plt.savetxt('na300.txt', na300, fmt='%0.2f %f')
-
 na450 = numpy.array(property[450])
-plt.savetxt('na450.txt', na450, fmt='%0.2f %f')
-
 na640 = numpy.array(property[640])
-plt.savetxt('na640.txt', na640, fmt='%0.2f %f')
-
-#plt.axis('tight')
-plt.subplot(221)
-y = na270[:,avgIndex]
-err = na270[:,stdIndex]
-title('270')
-ylabel('sampling blocks')
-plt.errorbar(range(1,len(y)+1), y, yerr=err)
-
-plt.subplot(222)
-y = na300[:,avgIndex]
-err = na300[:,stdIndex]
-title('300')
-ylabel('sampling blocks')
-plt.errorbar(range(1,len(y)+1), y, yerr=err)
+	
+if config.get('outputting', 'txt'):
+	plt.savetxt('na270.txt', na270, fmt='%0.2f %f')
+	plt.savetxt('na300.txt', na300, fmt='%0.2f %f')
+	plt.savetxt('na450.txt', na450, fmt='%0.2f %f')
+	plt.savetxt('na640.txt', na640, fmt='%0.2f %f')
 
 
-#plt.subplot(223)
-#y = na350[:,avgIndex]
-#err = na350[:,stdIndex]
-#plt.errorbar(range(1,len(y)+1), y, yerr=err)
+if config.get('outputting', 'plot'):
+	#set plot settings
+	if config.get('plotting', 'axes_tight'):
+		plt.axis('tight')
+	plt.grid(b=config.get('plotting', 'grid'), which='both')	
+	plt.rcParams['savefig.dpi'] = config.get('plotting', 'resolution')
+	plt.rcParams['font.size'] = config.get('plotting', 'font_size')
 
-plt.subplot(223)
-y=na450[:,avgIndex]
-err=na450[:,stdIndex]
-plt.errorbar(range(1,len(y)+1), y, yerr=err)
-
-plt.savefig(pname)
-
+	#plot
+	plt.subplot(221)
+	plot_temp(plt, '270', na270)
+	plt.savefig('testtest.png')
