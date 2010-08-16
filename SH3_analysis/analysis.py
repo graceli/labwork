b64fa8e9e0ab2d2c4229c71095800c6935ecc770
Grace Li <grace@abel.(none)>
2010-08-16 11:17:55 -0400

    update

--- SH3_analysis/analysis.py
+++ SH3_analysis/analysis.py
@@ -9,6 +9,8 @@ import matplotlib.pylab as plt
 import ConfigParser
 import os
 
+analysisParams = {'txtext':'csv', 'figext':'png'}
+
 def parseConfig():
 	#look for a configuration file in current directory
 	#if not found then looking of ~/.analysisrc
@@ -25,82 +27,151 @@ def parseConfig():
 	default, "will be used to set the entire analysis settings"
 
 	return config
+	
+def setConfig(config):
+	global analysisParams
+	
+	if config.get('outputting', 'txt'):
+		txtformat = config.get('formatting', 'text')
+		analysisParams['txtext'] = txtformat
+		
+	if config.get('outputting', 'plot'):
+		#set plot settings
+		if config.get('plotting', 'axes_tight'):
+			plt.axis('tight')
 
-def plot_temp(plt, temp, na):
+		plt.grid(b=config.get('plotting', 'grid'), which='both')	
+		plt.rcParams['savefig.dpi'] = config.get('plotting', 'resolution')
+		plt.rcParams['font.size'] = config.get('plotting', 'font_size')
+		figformat = config.get('formatting', 'figure')
+		analysisParams['figext'] = figformat
+
+def plot_by_parts(na270, na300, na450, na640):
+	if config.get('outputting', 'txt'):
+		plt.savetxt('na270.' + analysisParams['txtext'], na270, fmt='%0.6f %f')
+		plt.savetxt('na300.' + analysisParams['txtext'], na300, fmt='%0.6f %f')
+		plt.savetxt('na450.' + analysisParams['txtext'], na450, fmt='%0.6f %f')
+		plt.savetxt('na640.' + analysisParams['txtext'], na640, fmt='%0.6f %f')
+
+	if config.get('outputting', 'plot'):
+		#plot hardcoded settings
+		plt.subplots_adjust(top=0.95,hspace=0.4)
+
+		plt.subplot(221)
+		plot_vector(plt, '270', na270)
+		plt.subplot(222)
+		plot_vector(plt, '300', na300)
+		plt.subplot(223)
+		plot_vector(plt, '450', na450)
+		plt.subplot(224)
+		plot_vector(plt, '640', na640, 'sampling blocks')
+		plt.savefig(pname + '.' + analysisParams['figext'])
+
+def plot_hist(data):
+	if config.get('outputting', 'plot'):
+		pdf, bins, patches = plt.hist(data[300], arange(data[300].min(),
+data[300].max(), 0.05), normed=True, facecolor='green', alpha=0.75)
+	#n, bins, patches = plt.hist(x, 50, normed=1, facecolor='green', alpha=0.75)
+	
+	if config.get('outputting', 'txt'):
+		s= numpy.vstack((pdf,bins[0:len(bins)-1])).transpose()
+		plt.savetxt(pname+'_distribution' + analysisParams['txtext'], s,
+fmt='%f %f')
+
+def plot_vector(plt, title, na, label=''):
+	#plt is the plot object
+	#title is the plot title
+	#na is the numpy array to be plotted
+	
 	avgIndex = 0
 	stdIndex = 1
 	y = na[:,avgIndex]
 	err = na[:,stdIndex]
-	plt.title(temp)
-	plt.xlabel('sampling blocks')
+	plt.title(title)
 	plt.errorbar(range(1,len(y)+1), y, yerr=err)
+	plt.xlabel(label)
+	
+def plot_by_temp(by_temp):
+	plt.savetxt('by_temp.csv', by_temp, fmt='%f %f')
+	plot_vector(plt, 'Rg(Temperature)', by_temp, 'Temperature')
+	plt.savefig('by_temp.png')
+	
+if __name__ == "__main__":
+	if len(sys.argv) < 3:
+		print "usage: analysis.py path propertyname"
+		sys.exit(0)
 
+	config = parseConfig()
+	setConfig(config)
 
-if len(sys.argv) < 3:
-	print "usage: analysis.py path propertyname"
-	sys.exit(0)
+	path = sys.argv[1]
+	group, tablename, column = path.split('/')[1:]
+	print group, tablename, column, path
+	pname = sys.argv[2]
 
-config = parseConfig()
-path = sys.argv[1]
-group, tablename, column = path.split('/')[1:]
-print group, tablename, column, path
-pname = sys.argv[2]
+	filelist = glob.glob("*.h5")
 
-filelist = glob.glob("*.h5")
+	assert len(filelist) > 0
 
-assert len(filelist) > 0
+	Tlist = config.get('system-specific', 'temps').split()
+	#numpy.genfromtxt('templist.txt')
+	print Tlist
 
-Tlist = config.get('system-specific', 'temps').split()
-#numpy.genfromtxt('templist.txt')
-print Tlist
+	count = 0
+	start = 1
+	end = 41
 
-count = 0
-start = 1
-end = 41
+	by_parts = {}
+	data = {}
+	for i in range(start, end):
+		analysisfile = "analysis_part"+str(i)+".h5"
 
-property = {}
-for i in range(start, end):
-	analysisfile = "analysis_part"+str(i)+".h5"
+		if not os.path.exists(analysisfile):
+			print analysisfile, "does not exist"
+			continue
 
-	if not os.path.exists(analysisfile):
-		print analysisfile, "does not exist"
-		continue
+		print "processing", analysisfile
+		file = tables.openFile(analysisfile)
+		atable = file.getNode('/'+group+'/'+tablename)
+		
+		for Tstr in Tlist:
+			T = int(Tstr)
+			datachunk = atable.readWhere('temp==%(T)d' % vars())[column]
+			
+			# add to or extend the current list with the data read from a table
+			if T not in data:
+				data[T] = datachunk
+			else:
+				data[T].extend(datachunk)
+			
+			# do per analysis file aggregation
+			if T not in by_parts:
+				by_parts[T] = [[numpy.average(datachunk), numpy.std(datachunk)]]
+			else:
+				by_parts[T].append([numpy.average(datachunk), \
+				numpy.std(datachunk)])
 
-	print "processing", analysisfile
-	file = tables.openFile(analysisfile)
-	table = file.getNode('/'+group+'/'+tablename)
-	data = []
+		
+		#close the h5 file
+		file.close()
+
+	by_temp = {}
 	for Tstr in Tlist:
 		T = int(Tstr)
-		data = table.readWhere('temp==%(T)d' % vars())[column]
-		if T not in property:
-			property[T] = [[numpy.average(data), numpy.std(data)]]
+		if T not in by_temp:
+			by_temp[T] = [[numpy.average(data[T]), numpy.std(data[T])]]
 		else:
-			property[T].append([numpy.average(data), numpy.std(data)])
+			by_temp[T].append([numpy.average(data[T]), numpy.std(data[T])])
 
-	count+=1
+	na270 = numpy.array(by_parts[270])
+	na300 = numpy.array(by_parts[300])
+	na450 = numpy.array(by_parts[450])
+	na640 = numpy.array(by_parts[640])
 
-na270 = numpy.array(property[270])
-na300 = numpy.array(property[300])
-na450 = numpy.array(property[450])
-na640 = numpy.array(property[640])
-	
-if config.get('outputting', 'txt'):
-	plt.savetxt('na270.txt', na270, fmt='%0.2f %f')
-	plt.savetxt('na300.txt', na300, fmt='%0.2f %f')
-	plt.savetxt('na450.txt', na450, fmt='%0.2f %f')
-	plt.savetxt('na640.txt', na640, fmt='%0.2f %f')
+	plot_by_parts(na270,na300,na450,na640)
+	plot_hist(data)
+	plot_by_temp(by_temp)
 
 
-if config.get('outputting', 'plot'):
-	#set plot settings
-	if config.get('plotting', 'axes_tight'):
-		plt.axis('tight')
-	plt.grid(b=config.get('plotting', 'grid'), which='both')	
-	plt.rcParams['savefig.dpi'] = config.get('plotting', 'resolution')
-	plt.rcParams['font.size'] = config.get('plotting', 'font_size')
 
-	#plot
-	plt.subplot(221)
-	plot_temp(plt, '270', na270)
-	plt.savefig('testtest.png')
+
