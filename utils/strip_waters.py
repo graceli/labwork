cb96a8d1a98269e1c9bfb410060cc5cf81219efe
Grace Li <grace@abel.(none)>
2010-08-23 11:16:53 -0400

    update strip_waters

--- utils/strip_waters.py
+++ utils/strip_waters.py
@@ -8,36 +8,6 @@ from optparse import OptionParser
 
 usage = """%prog [options] <tprfile> <xtcfile> """
 
- #usage = """
-        #usage: %prog [options] <PSF file> <PDB file> <DCD file>
-    #"""
-    
-    # initialize the parser with our custom usage string (above)
-    #parser = optparse.OptionParser(usage)
-    ## add all the command-line options
-    #parser.add_option("-o", dest="h5_filename", default="analysis.h5",
-#help="Output analysis file [default: %default]")    
-    #parser.add_option("-s", dest="selection", default="name CA", help="Atom
-#selection [default: %default]")    
-    #parser.add_option("-t", dest="first_timestep", default=0, type="int",
-#help="Starting timestep [default: %default]")    
-    #parser.add_option("-f", dest="dcdtime", default=1, type="int", help="DCD
-#output frequency [default: %default]")    
-    #parser.add_option("-d", dest="dt", default=0.002, type="float",
-#help="Integration Timestep [default: %default]")    
-    
-    ## parse the command line
-    #(options, args) = parser.parse_args()
-    
-    ## check to make sure all arguments are there
-    #if len(args) < 3:
-        #parser.error("No input files specified")
-    
-    #psf_file = args[0]
-    #pdb_file = args[1]
-    #dcd_file = args[2]
-
-
 parser = OptionParser(usage)
 
 parser.add_option("-o", dest="outputFilename", default="nosol.xtc",
@@ -49,16 +19,18 @@ if len(args)<2:
 	parser.error("Missing input files")
 
 tprfile = args[0]
-xtcfile = args[1]
+files = args[1]
 
 if not os.path.exists(tprfile):
 	print "Error: the tpr file was not found"
 	sys.exit(0)
 
-if not os.path.exists(xtcfile):
-	print "Error: the xtc file was not found"
+if len(files) < 1:
+	print "Error: the xtc file(s) was not specified"
 	sys.exit(0)
 
+filesList = glob.glob(files)
+print filesList
 
 #create a no nosolvent index file
 selection = '!"SOL"'
@@ -66,8 +38,10 @@ command = "make_ndx -f %s -n %s" % (tprfile, outputIndex)
 returnCode = subprocess.Popen(shlex.split(command),
 stdin=subprocess.PIPE).communicate("%s" % (selection))
 
-selection = '!SOL'
-command = "trjconv -f %s -n %s -o %s" % (tprfile, xtcfile,
-parser.outputFilename)
-returnCode = subprocess.Popen(shlex.split(command),
-stdin=subprocess.PIPE).communicate("%s" % (selection))
+for xtcfile in xtcList:
+	selection = '!SOL'
+	command = "trjconv -f %s -n %s -o %s" % (tprfile, xtcfile,
+	parser.outputFilename)
+	returnCode = subprocess.Popen(shlex.split(command),
+	stdin=subprocess.PIPE).communicate("%s" % (selection))
+
