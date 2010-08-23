a55cdb0ba1d2d3a1f75cb37cd6768087f7a513e9
Grace Li <grace@gpc-f102n084.(none)>
2010-08-23 12:40:26 -0400

    make_ndx selection broken

--- utils/strip_waters.py
+++ utils/strip_waters.py
@@ -4,13 +4,16 @@
 import os
 import subprocess
 import shlex
+import glob
+import sys
+
 from optparse import OptionParser
 
 usage = """%prog [options] <tprfile> <xtcfile> """
 
 parser = OptionParser(usage)
 
-parser.add_option("-o", dest="outputFilename", default="nosol.xtc",
+parser.add_option("-o", dest="outputFilename", default="nosol",
 help="specify the output trajectory file name")
 
 (options, args) = parser.parse_args()
@@ -29,19 +32,22 @@ if len(files) < 1:
 	print "Error: the xtc file(s) was not specified"
 	sys.exit(0)
 
-filesList = glob.glob(files)
-print filesList
+print files
+xtcList = glob.glob(files)
+print xtcList
 
 #create a no nosolvent index file
-selection = '!"SOL"'
-command = "make_ndx -f %s -n %s" % (tprfile, outputIndex)
-returnCode = subprocess.Popen(shlex.split(command),
-stdin=subprocess.PIPE).communicate("%s" % (selection))
+# I can't get make_ndx to read the \n properly here
+#selection = '!"SOL"'
+#command = "make_ndx -f %s -o %s" % (tprfile, options.outputFilename)
+#returnCode = subprocess.Popen(shlex.split(command), stdin=subprocess.PIPE).communicate("%s%s%s" % (selection,"\n", 'q'))
+#print returnCode
+#sys.exit(0)
 
 index=0
 for xtcfile in xtcList:
 	selection = '!SOL'
-	command = "trjconv -f %s -n %s -o %s" % (tprfile, xtcfile, str(index)+"_nosol")
+	command = "trjconv -f %s -s %s -n %s -o %s" % (xtcfile, tprfile, options.outputFilename, str(index)+"_nosol")
 	returnCode = subprocess.Popen(shlex.split(command),
 	stdin=subprocess.PIPE).communicate("%s" % (selection))
 	index += 1
