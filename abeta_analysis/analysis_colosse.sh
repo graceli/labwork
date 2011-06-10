656385db606666d2a4bfb76c4ff3641671ec24fd
Grace Li <fullofgrace88@gmail.com>
2011-06-09 20:50:32 -0400

    added chain_hbonds and dssp

--- abeta_analysis/analysis_colosse.sh
+++ abeta_analysis/analysis_colosse.sh
@@ -6,100 +6,80 @@
 #set -e
 set -x
 
-# function dssp {
-#         for ratio in 15 64; do
-#                 for iso in scyllo chiro; do
-#                         trj="ab_${ratio}_${iso}.xtc_c_fit.xtc"
-#                         echo 1 | do_dssp -f $ratio/$trj -s $ratio/nosol.tpr -o ab_${ratio}_${iso}_ss -sc ab_${ratio}_${iso}_sc -dt 10
-#                         trap "exit 1" TERM INT KILL
-#                 done
-#         done
+trap 'exit' TERM INT SIGINT EXIT SIGKILL SIGSTOP SIGTERM
+
+# res_start=0
+# res_end=129
+# INS_grp=130
+# num=0
+# function hbonds {
+# 	iso=$1
+#     ratio=$2
+# 	output_dir=$3/hbonds
+# 	mkdir -p $output_dir
+# 		
+# 	for s in `seq 1 10`; do
+# 		xtc="ab_${iso}_${ratio}_${s}_nosol_whole.xtc_c_fit"
+# 		if [ -e "$DATA/${xtc}.xtc" ]; then
+# 			mkdir -p $output_dir/$s
+# 			seq $res_start $res_end | parallel -j 8 "echo {} $INS_grp | g_hbond -f $DATA/$xtc -s ${iso}_${ratio}_nosol.tpr -n g_hbond_${ratio}_${iso}.ndx -nonitacc -nomerge -num $output_dir/$s/{} $xvgr $TEST > /dev/null 2>&1"
+# 		fi
+# 		# python /home/grace/AnalysisScripts/abeta_analysis/abeta_analysis.py sys${s}.h5
+# 	done
+# 	clean "${iso}_${ratio}_hbonds"
 # }
 
-trap 'exit' TERM INT SIGINT EXIT SIGKILL SIGSTOP SIGTERM
+# proteingrp=1
+# insgrp=12
+# chain1=17
+# chain2=18
+# chain3=19
+# chain4=20
+# chain5=21
+# xvgr="-noxvgr"
+# # start=1
+# # end=20
+# function nonpolar {
+# 	iso=$1
+#     ratio=$2
+# 	output_dir=$3/nonpolar
+# 	mkdir -p $output_dir
+# 	
+# 	 echo -e "'SideChain'&aC*&!rACE\nsplitch16\nq" | make_ndx -f ${iso}_${ratio}_nosol.tpr -o ab_${ratio}_nonpolar.ndx
+# 	
+# 	for s in `seq 1 10`; do
+# 		xtc="ab_${iso}_${ratio}_${s}_nosol_whole.xtc_c_fit"
+# 		if [ -e "$DATA/${xtc}.xtc" ]; then
+# 			seq $chain1 $chain5 | parallel -j 5 "echo {} $insgrp | g_inositol_residue_nonpolar_v2 -f $DATA/$xtc -s ${iso}_${ratio}_nosol.tpr -n ab_${ratio}_nonpolar.ndx -per_residue_contacts $output_dir/chain{}_residue_np_contact.dat -per_inositol_contacts $output_dir/chain{}_inositol_np_contact.dat -per_residue_table chain{}_table.dat $TEST"
+# 		fi
+# 	done
+# 	clean "${iso}_${ratio}_nonpolar"
+# }
+
+
+function dssp {
+	echo 1 | do_dssp -f $NAME -s $TPR -o ${NAME}_ss -sc ${NAME}_sc
+}
 
 chain_start=0
 chain_end=3
 function chain_hbonds {
-	iso=$1
-    ratio=$2
-	output_dir=$3/hbonds
-	mkdir -p $output_dir
-		
-	for s in `seq 1 10`; do
-		xtc="ab_${iso}_${ratio}_${s}_nosol_whole.xtc_c_fit"
-		if [ -e "$DATA/${xtc}.xtc" ]; then
-			mkdir -p $output_dir/$s
-			for ch in `seq $chain_start $chain_end`; do
-				let next=ch+1
-				echo $ch $next | g_hbond -f $DATA/$xtc -s ${iso}_${ratio}_nosol.tpr -n chain.ndx -nonitacc -nomerge -num $output_dir/$s/chain_${ch}_${next}_hbonds $TEST > /dev/null 2>&1 &
-			done
-			wait
-		fi
-		# python /home/grace/AnalysisScripts/abeta_analysis/abeta_analysis.py sys${s}.h5
+	xtc="ab_${iso}_${ratio}_${s}_nosol_whole.xtc_c_fit"
+	for ch in {$chain_start..$chain_end}; do
+		let next=ch+1
+		echo $ch $next | g_hbond -f $NAME -s $TPR -n chain.ndx -nonitacc -nomerge -num chain_${ch}_${next}_hbonds $TEST
 	done
-	clean "${iso}_${ratio}_chain_hbonds"
-}
-
-res_start=0
-res_end=129
-INS_grp=130
-num=0
-function hbonds {
-	iso=$1
-    ratio=$2
-	output_dir=$3/hbonds
-	mkdir -p $output_dir
-		
-	for s in `seq 1 10`; do
-		xtc="ab_${iso}_${ratio}_${s}_nosol_whole.xtc_c_fit"
-		if [ -e "$DATA/${xtc}.xtc" ]; then
-			mkdir -p $output_dir/$s
-			seq $res_start $res_end | parallel -j 8 "echo {} $INS_grp | g_hbond -f $DATA/$xtc -s ${iso}_${ratio}_nosol.tpr -n g_hbond_${ratio}_${iso}.ndx -nonitacc -nomerge -num $output_dir/$s/{} $xvgr $TEST > /dev/null 2>&1"
-		fi
-		# python /home/grace/AnalysisScripts/abeta_analysis/abeta_analysis.py sys${s}.h5
-	done
-	clean "${iso}_${ratio}_hbonds"
-}
-
-
-proteingrp=1
-insgrp=12
-chain1=17
-chain2=18
-chain3=19
-chain4=20
-chain5=21
-xvgr="-noxvgr"
-# start=1
-# end=20
-function nonpolar {
-	iso=$1
-    ratio=$2
-	output_dir=$3/nonpolar
-	mkdir -p $output_dir
-	
-	 echo -e "'SideChain'&aC*&!rACE\nsplitch16\nq" | make_ndx -f ${iso}_${ratio}_nosol.tpr -o ab_${ratio}_nonpolar.ndx
-	
-	for s in `seq 1 10`; do
-		xtc="ab_${iso}_${ratio}_${s}_nosol_whole.xtc_c_fit"
-		if [ -e "$DATA/${xtc}.xtc" ]; then
-			seq $chain1 $chain5 | parallel -j 5 "echo {} $insgrp | g_inositol_residue_nonpolar_v2 -f $DATA/$xtc -s ${iso}_${ratio}_nosol.tpr -n ab_${ratio}_nonpolar.ndx -per_residue_contacts $output_dir/chain{}_residue_np_contact.dat -per_inositol_contacts $output_dir/chain{}_inositol_np_contact.dat -per_residue_table chain{}_table.dat $TEST"
-		fi
-	done
-	clean "${iso}_${ratio}_nonpolar"
 }
 
 # calculate the rmsf
 function rmsf {
     # backbone fitting for specific parts of the peptide
-	echo "C-alpha" | g_rmsf -f $DATA/sys${SYS}_${TAG}.xtc -s $DATA/protein_sugar.tpr -o sys${SYS}_${TAG}_rmsf.xvg -fit -res -ox sys${SYS}_${TAG}_ox.xvg -noxvgr $TEST 
+	echo "C-alpha" | g_rmsf -f $NAME -s $TPR -o sys${SYS}_${TAG}_rmsf.xvg -fit -res -ox sys${SYS}_${TAG}_ox.xvg -noxvgr $TEST 
 }
 
 # calculate the rmsd of the protein using the nmr structure as a reference
 function rmsd {
-	echo 1 1 | g_rms -f $DATA/sys${SYS}_${tag}.xtc -s $DATA/protein_sugar.tpr -o sys${SYS}_whole_rmsd_protein.xvg -noxvgr $TEST
-	# echo 4 4 | g_rms -f $DATA/sys{}_nosol_whole.xtc -s protein_sugar.tpr -o $output_dir/sys{}_whole_rmsd_backbone.xvg -noxvgr $TEST
+	echo 1 1 | g_rms  -o sys${SYS}_whole_rmsd_protein.xvg -noxvgr $TEST
 }
 
 base_dir=`pwd`
@@ -108,10 +88,11 @@ DATA="/rap/uix-840-ac/grace/abeta/42/glucose/xtc"
 # SYS=$SYSN
 # ANALYSIS=$ANALY
 SYS=1
-ANALYSIS=rmsf
+ANALYSIS=chain_hbonds
 TEST="-dt 1000"
 TAG="nosol_whole"
-
+NAME="$DATA/sys${SYS}_${tag}.xtc"
+TPR="$DATA/protein_sugar.tpr"
 echo "For system $SYS"
 echo "Performing analysis $ANALYSIS"
 
