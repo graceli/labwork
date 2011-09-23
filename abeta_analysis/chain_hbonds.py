8e3d723ada1fdb993cc74c91016da267e84b003c
Grace Li <grace.li@utoronto.ca>
2011-09-23 17:38:09 -0400

    abeta_analysis: reorganized files and combined functions common to a single analysis into one file (chain_hbond.py)

--- abeta_analysis/chain_hbonds.py
+++ abeta_analysis/chain_hbonds.py
@@ -1,6 +1,23 @@
 import sys
 import numpy
 import os
+import glob
+
+
+def comment_xvgr():
+	# comment out the xvgr header lines for the chain chain hbond analysis
+	for i in range(1,11):
+		for chain in range(0,4):
+			next_chain = chain + 1
+			os.system("sed -e 's/@/#/g' %(i)d/chain_%(chain)d_%(next_chain)d_hbonds.xvg > chch_hbonds/sys%(i)d_chain_%(chain)d_%(next_chain)d_hbonds.dat" % vars())
+
+	# delete the data files for very short (failed) simulation trajs
+	files = glob.glob('chch_hbonds/*.dat')
+	for f in files:
+		size = os.path.getsize(f)/(1024.0*1024)
+		if size < 1:
+			os.system('rm %(f)s' % vars())
+
 
 def chain_hbonds(subplot_num):
 	average = []
