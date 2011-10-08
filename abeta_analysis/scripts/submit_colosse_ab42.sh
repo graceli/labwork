a6584045c02c61da193a75a955834ae86edb20ed
Grace Li <fullofgrace88@gmail.com>
2011-10-08 01:55:10 -0400

    abeta_analysis: updated the colosse submit script for glucose (redo after delete)

--- abeta_analysis/scripts/submit_colosse_ab42.sh
+++ abeta_analysis/scripts/submit_colosse_ab42.sh
@@ -29,35 +29,35 @@ echo "NRESUBMITS is $NRESUBMITS"
 base_dir=$PWD
 echo "starting run in $PWD"
 
-function clean {
-        cp -rp $SHM/{*.xtc,*.tpr,*.log,*.cpt,*.edr} $base_dir/sys${SGE_TASK_ID}
-	cd $base_dir
-        rm -rf $SHM
-}
-
 #-notify sends SIGUSR2 
 #trap "clean; exit 0" KILL INT SIGINT TERM EXIT SIGUSR2
 
+if [ ! -e "sys${SGE_TASK_ID}"]; then
+	mkdir sys$SGE_TASK_ID
+fi
 cd sys$SGE_TASK_ID
 
 cptfile=""
 if [ "$num" == "0" ]; then
-	grompp -f ../em.mdp -c sys${SGE_TASK_ID}_start.gro -p sys${SGE_TASK_ID}.top -o em.tpr
-	mdrun -s em.tpr -deffnm em
-	grompp -f ../production.mdp -c em.gro -p sys${SGE_TASK_ID}.top -o prod.tpr
-	#cp -p *.tpr $SHM
+	if [[ ! -e "sys${SGE_TASK_ID}_prod.cpt" || ! -s "sys${SGE_TASK_ID}_prod.cpt" ]]; then	
+		grompp -f $base_dir/equil_nvt.mdp -c em.gro -p $base_dir/abeta42.top -o sys${SGE_TASK_ID}_equil_nvt.tpr 
+		mpirun mdrun -deffnm sys${SGE_TASK_ID}_equil_nvt -s sys${SGE_TASK_ID}_equil_nvt.tpr -nosum -dlb yes -npme -1 -maxh 47
+		
+		grompp -f $base_dir/equil_npt.mdp -c sys${SGE_TASK_ID}_equil_nvt -p $base_dir/abeta42.top -o sys${SGE_TASK_ID}_equil_npt
+		mpirun mdrun -deffnm sys${SGE_TASK_ID}_equil_npt -s sys${SGE_TASK_ID}_equil_npt -nosum -dlb yes -npme -1 -maxh 47
+
+		grompp -f $base_dir/abeta42_prod.mdp -c sys${SGE_TASK_ID}_equil_npt -p $base_dir/abeta42.top -o sys${SGE_TASK_ID}_prod
+	fi
 else
-	#cp -p *.cpt *.tpr $SHM
-	cptfile="$base_dir/sys${SGE_TASK_ID}/sys${SGE_TASK_ID}_prod.cpt"
+	cptfile="$base_dir/${SGE_TASK_ID}/sys${SGE_TASK_ID}_prod.cpt"
 fi
 
-#cd $SHM
-mpirun /software/apps/gromacs-4.0.7/bin/mdrun -deffnm sys${SGE_TASK_ID}_prod -s $base_dir/sys${SGE_TASK_ID}/prod.tpr -nosum -dlb yes -npme -1 -cpi $cptfile -cpt 60 -maxh 47
+mpirun mdrun -deffnm sys${SGE_TASK_ID}_prod -s $base_dir/sys${SGE_TASK_ID}/prod.tpr -nosum -dlb yes -npme -1 -cpi $cptfile -cpt 60 -maxh 47
 
 # resubmit
-#if [ "$num" -lt "$NRESUBMITS" ]; then
+# if [ "$num" -lt "$NRESUBMITS" ]; then
 #    num=$(($num+1))
 #    echo "resubmitting - sequence $num for replica $SGE_TASK_ID"
 #    ssh colosse1 "cd $base_dir; qsub -v NUM=$num,SGE_TASK_ID=${SGE_TASK_ID} -N ${JOB_NAME}_${SGE_TASK_ID}_${num} ./run.sh"
-#fi
+# fi
 
