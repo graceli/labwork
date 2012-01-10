1a1bff30149e54ad809d328aaed390d253fb4383
Grace Li <grace.li@utoronto.ca>
2012-01-10 12:31:00 -0500

    Added the beginnings of trjcat for sh3

---
+++ SH3_analysis/extract/test.py
@@ -0,0 +1,25 @@
+import tarfile
+import os
+
+# Return the xtc files in the tarfile as a set
+def xtcfiles(tarfilename):
+    tar = tarfile.open(tarfilename)
+    members = tar.getmembers()
+    files = set([])
+    for info in members:
+        if os.path.splitext(info.name)[1] == '.xtc':
+            files.add(info.name)
+    return files
+
+def main():
+    # build an in memory dictionary of tarfiles
+    tarfiles_dict = {}
+    for each tar file:
+        tarfiles_dict[tarfile] = xtcfiles(tarfile)
+    
+if __name__ == '__main__':
+    files = xtcfiles('lgw124.90188-lgw35.91189.tmp.455')
+    print len(files)
+    name = 'lgw124.910660000000000000_small.xtc'
+    if name in files:
+        print name, "is in tar file"
