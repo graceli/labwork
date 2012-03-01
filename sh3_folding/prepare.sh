b359c9eb657f19a956d0da7ddb48b44800a8f8c0
Grace Li <grace.li@utoronto.ca>
2012-03-01 02:38:43 -0500

    fix for prepare.sh script

--- sh3_folding/prepare.sh
+++ sh3_folding/prepare.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-function make_check_dir() {
-    mkdir runs_${NAME}
+function make_systems {
     NAME=$1
     START=$2
     END=$3
+	
+	if [ ! -e "runs_$NAME" ]; then
+		mkdir runs_${NAME}
+	fi
+
     for i in `seq $START $END`; do
         mkdir runs_${NAME}/${i}
-        cp ${NAME}_ions_final.gro runs_${NAME}/${i}/${NAME}.gro
-        cp ${NAME}.top ${NAME}/sys${i}/${i}/${NAME}.top
-        grompp -f params/em.mdp -c  runs_${NAME}/${i}/${NAME}.gro -o runs_${NAME}/${i}/em.tpr -p runs_${NAME}/sys${i}/${i}/${NAME}.top
+        cp initial/${NAME}_ions_final.gro runs_${NAME}/${i}/${NAME}_${i}.gro
+        cp initial/${NAME}.top runs_${NAME}/${i}/${NAME}_${i}.top
+        grompp -f params/em.mdp -c  runs_${NAME}/${i}/${NAME}_${i}.gro -o runs_${NAME}/${i}/em.tpr -p runs_${NAME}/${i}/${NAME}_${i}.top -po ${i}
     done
 }
 
-make_systems "max_mrsd" 1 1
-make_systems "max_q" 1 1
+make_systems "max_mrsd" 1 100
+make_systems "max_q" 1 100
