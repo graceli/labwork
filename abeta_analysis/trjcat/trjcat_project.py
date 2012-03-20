f170c89a3063ba58ef1e9dffef47ae1cc531ba32
Grace Li <fullofgrace88@gmail.com>
2012-03-20 00:15:31 -0400

    Added more skeleton code for trjcat.  Don't think the code runs. Doing bad things by commiting bad code

--- abeta_analysis/trjcat/trjcat_project.py
+++ abeta_analysis/trjcat/trjcat_project.py
@@ -3,25 +3,70 @@ import os
 import sys
 import glob
 
-class Trajectory: 
-    # for each directory i
-        # files = read_xtc_files(directory)
-        # trjcat -f " ".join(files) -n project_directory/index_file -o tempfile_i
-        # trjconv -f tempfile_i -pbc mol -o final_i
+class GromacsCommand:
+    # how do pass in multiple kwargs?
+    def __init__(self, executable, args):
+        self.gromacs_exe = executable
+        self.gromacs_args = args
+        
+    def run():
+        # run command
+        # wrapper around 
+        pass
     
-    def __init__(self, dir_idx, files, group):
-        self.dir_name = dir_idx
-        self.files_to_cat = files
-        self.system_group = group
+    # does python have some sort of default to_string method for a class that I can override ?
+    # like in ruby or Java?
+    def to_string():
+        # returns the command as a string
+        # string version of what would be ran
+        pass
+        
+class Trajectory:
+    def __init__(self, name, dir_name, trajs):
+        # name of the single trajectory
+        self.name = name
+
+        # names of the trajectory pieces
+        self.files_to_cat = trajs
+
+        # location of the trajectories
+        self.dir_name = dir_name
     
-    def trjcat(self):
+    def build_traj(self, index_group):
         files_str = " ".join(self.files_to_cat)
-        # input is group                                                                    
-        idx_file_path = os.path.join(self.dir_name, "index.ndx")
-        command = "trjcat -f %s -o %s -n %s" % (files_str, self.dir_name, idx_file_path)
-        # subprocess.call
+        # input is index_group                                                                    
+        index_file = os.path.join(self.dir_name, "index.ndx")
+        temp_name = self.dir_name
+        command = "trjcat -f %s -o %s -n %s" % (files_str, temp_name, index_file)
+        args = shlex.split(command)
+        p = subprocess.Popen(args, stdout=os.devnull).communicate(index_group)
+        logging.info("%s executed", command)
+        
+        if p.returncode == None || p.returncode != 0:
+            raise CallProcessError
+        
+        final_name = temp_name + "_processed"
+        command = "trjconv -f %s -o %s -pbc mol" % (self.dir_name, final_name)
+        
+        logging.info("%s executed", command)
+        
+    def check_traj(self):
+        command = "gmxcheck -f %s" % (self.name)
         print command
 
+class Project:
+    def __init__(self, trjs):
+        self.trajectories = trjs 
+
+    def data_info(self):
+        # log a list of trajectories produced and their file sizes
+        # Project: ProjectName
+        # N directories trjcatted
+        # Group selected: "Non-Solvent"
+        # traj    size
+        # 1       100 M
+        # ...
+        
 def main():
     FORMAT = '%(asctime)s %(levelname)s %(message)s'
     logging.basicConfig(filename='trjcat_project.log', format=FORMAT, level=logging.DEBUG)
@@ -30,7 +75,7 @@ def main():
     # Usage: trjcat_project <project_name>
 
     # TODO error checking
-    # temporarily use a fixed project name
+    # TODO refactor to configuration file
     project_name = "TestProject"
     
     if not os.path.exists(project_name):
@@ -40,7 +85,7 @@ def main():
     logging.info("Initializing trjcatting for project %s", project_name)
     N = 10
 
-    # read project and build a list of files to trjcat
+    # Read project directory and build a list of files to trjcat
     files_for_trjcat = []
     for dir_idx in range(N):
         trj_dir_path = os.path.join(project_name, str(dir_idx))
@@ -59,15 +104,9 @@ def main():
     
 
     for t in files_for_trjcat:
-        t.trjcat()
-    
-    # log a list of trajectories produced and their file sizes
-    # Project: ProjectName
-    # N directories trjcatted
-    # Group selected: "Non-Solvent"
-    # traj    size
-    # 1       100 M
-    # ...
+        t.build_traj()
+        t.traj_info()
+
 
 if __name__ == '__main__':
     main()
