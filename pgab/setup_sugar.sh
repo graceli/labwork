73aec7a0be171767ef7e3aee967d91cfb3847131
Grace Li <ligrace1@ip13.m>
2012-03-22 20:27:10 -0400

    horrible non dry code for density analysis

--- pgab/setup_sugar.sh
+++ pgab/setup_sugar.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
 
 function make_box {
+    GRO=$1
     editconf -f $GRO -box 3 3 3 -o nag_box.gro
-    genbox -cp nag_box.gro -cs ~/gro/tip3.gro -p nag_glycam.top
+    genbox -cp nag_box.gro -cs ~/systems/gro/tip3.gro -p nag_glycam.top
 }
 
 make_box glycam_3_12_2012_752_GMX.gro
