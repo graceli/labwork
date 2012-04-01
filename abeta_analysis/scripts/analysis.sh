980d16372e4dea545ece52421b621fc799b56c9b
Grace Li <grace.li@utoronto.ca>
2012-03-31 22:35:19 -0400

    modified chain_hbonds, dssp, rmsf_calpha to work with the updated trajectories.  still need to test this on scinet

--- abeta_analysis/scripts/analysis.sh
+++ abeta_analysis/scripts/analysis.sh
@@ -1,27 +1,17 @@
 #!/bin/sh
-##PBS -l nodes=1:compute-eth:ppn=8,walltime=02:00:00,os=centos53computeA
+#PBS -l nodes=1:ppn=8,walltime=10:00:00
 #PBS -N analysis
 
 . ~/.gmx_407
 
 set -u
-#set -e
 set -x
+#set -e
 
 #extract the number of nonpolar contacts between inositol and residues
 # function make_indices {
 # 	echo -e "'SideChain'&aC*&!rACE\nsplitch17\nq" | make_ndx -f common/em.tpr -o common/ab_nonpolar.ndx 
 # 	RETURN_CODE=$?
-# }
-
-# function dssp {
-#         for ratio in 15 64; do
-#                 for iso in scyllo chiro; do
-#                         trj="ab_${ratio}_${iso}.xtc_c_fit.xtc"
-#                         echo 1 | do_dssp -f $ratio/$trj -s $ratio/nosol.tpr -o ab_${ratio}_${iso}_ss -sc ab_${ratio}_${iso}_sc -dt 10
-#                         trap "exit 1" TERM INT KILL
-#                 done
-#         done
 # }
 
 function clean {
@@ -32,7 +22,10 @@ function clean {
 }
 
 
+# Calculates the number of nonpolar contacts of solute to residue for each chain
+# In the aggregate.  Don't really remember why I did it like this
 proteingrp=1
+# insgrp can be glycerol or glucose
 insgrp=5
 chain1=0
 chain5=4
@@ -45,6 +38,7 @@ function nonpolar {
 	output_dir=$3/nonpolar
 	mkdir -p $output_dir
 	
+    # We don't need make the index over and over again for the peptides, because they are all the same
 	 #echo -e "'SideChain'&aC*&!rACE\nsplitch16\nq" | make_ndx -f ${iso}_${ratio}_nosol.tpr -o ab_${ratio}_nonpolar.ndx
 	
 	for s in `seq 0 9`; do
@@ -68,6 +62,9 @@ function rmsd {
     clean "${iso}_${ratio}_rmsd"
 }
 
+# Calculates the number of hydrogen bonds made with each residue.
+# To calculate this I run g_hbond each time for each residue in the pentamer
+# This is a bit of a hack to get g_hbond to work.
 res_start=0
 res_end=129
 INS_grp=130
@@ -91,64 +88,61 @@ function hbonds {
     clean "${iso}_${ratio}_hbonds"
 }
 
+# TODO test this
+chain_start=0
+chain_end=3
+function chain_hbonds {
+    iso=$1
+    ratio=$2
+    output_dir=$3/hbonds
+    mkdir -p $output_dir
+  
+    for s in `seq 0 9`; do
+      xtc="${s}_final"
+      if [ -e "$DATA/${xtc}.xtc" ]; then
+          mkdir -p $output_dir/$s
+          for ch in `seq $chain_start $chain_end`; do
+              let next=ch+1
+              echo $ch $next | g_hbond -f $DATA/$xtc -s ${iso}_${ratio}_nosol.tpr -n chain.ndx -nonitacc -nomerge -num $output_dir/$s/chain_${ch}_${next}_hbonds $TEST > /dev/null 2>&1 &
+          done
+          wait
+      fi
+      # python /home/grace/AnalysisScripts/abeta_analysis/abeta_analysis.py sys${s}.h5
+    done
+    clean "${iso}_${ratio}_chain_hbonds"
+}
 
-# function dssp {
-#   export DSSP=/home/grace/src/dssp_ana/dsspcmbi
-#   
-#   for i in `seq 0 10`; do 
-#       if [ ! -e "$SHM/$i" ]; then
-#           mkdir -p $SHM/$i
-#       fi
-#   done
-# 
-#   seq 0 9 | parallel -j 8 "cd $SHM/{}; echo 1 | do_dssp -f $DATA/ab_${ISO}_${RATIO}_{}_nosol_whole.xtc_c_fit -s $base_dir/${ISO}_${RATIO}_nosol.tpr -o ab_${ISO}_${RATIO}_{}_ss -sc ab_${ISO}_${RATIO}_{}_sc $TEST 2>&1"
-#   
-#   clean "${ISO}_${RATIO}_dssp"
-# }
+# TODO test this
+function dssp {
+    export DSSP=/home/grace/src/dssp_ana/dsspcmbi
+    
+    for i in `seq 0 9`; do 
+      if [ ! -e "$SHM/$i" ]; then
+          mkdir -p $SHM/$i
+      fi
+    done
 
+    seq 0 9 | parallel -j 8 "cd $SHM/{}; echo 1 | do_dssp -f $DATA/{}_final -s $base_dir/${ISO}_${RATIO}_nosol.tpr -o ab_${ISO}_${RATIO}_{}_ss -sc ab_${ISO}_${RATIO}_{}_sc $TEST 2>&1"
 
-# chain_start=0
-# chain_end=3
-# function chain_hbonds {
-#   iso=$1
-#     ratio=$2
-#   output_dir=$3/hbonds
-#   mkdir -p $output_dir
-#       
-#   for s in `seq 1 10`; do
-#       xtc="ab_${iso}_${ratio}_${s}_nosol_whole.xtc_c_fit"
-#       if [ -e "$DATA/${xtc}.xtc" ]; then
-#           mkdir -p $output_dir/$s
-#           for ch in `seq $chain_start $chain_end`; do
-#               let next=ch+1
-#               echo $ch $next | g_hbond -f $DATA/$xtc -s ${iso}_${ratio}_nosol.tpr -n chain.ndx -nonitacc -nomerge -num $output_dir/$s/chain_${ch}_${next}_hbonds $TEST > /dev/null 2>&1 &
-#           done
-#           wait
-#       fi
-#       # python /home/grace/AnalysisScripts/abeta_analysis/abeta_analysis.py sys${s}.h5
-#   done
-#   clean "${iso}_${ratio}_chain_hbonds"
-# }
-# 
+    clean "${ISO}_${RATIO}_dssp"
+}
 
+# TODO test this
+# calculate the rmsf
+function rmsf_calpha {
+    iso=$1
+    ratio=$2
+    output_dir=$3/rmsf
+    mkdir -p $output_dir
+    calpha=3
 
-# 
-# # calculate the rmsf
-# function rmsf_calpha {
-#   iso=$1
-#     ratio=$2
-#   output_dir=$3/rmsf
-#   mkdir -p $output_dir
-#     calpha=3
-# 
-#     # backbone fitting for specific parts of the peptide    
-#   seq 1 10 | parallel -j 8 "echo $calpha | g_rmsf -f $DATA/ab_${iso}_${ratio}_{}_nosol_whole.xtc_c_fit.xtc -s ${iso}_${ratio}_nosol.tpr -o $output_dir/${iso}_${ratio}_{}_rmsf.xvg -fit -res -ox $output_dir/ab_${iso}_${ratio}_{}_nosol_whole.xtc_c_fit -noxvgr $TEST" 
-#   clean "${iso}_${ratio}_rmsf"
-# }
+    # backbone fitting for specific parts of the peptide
+    seq 0 9 | parallel -j 8 "echo $calpha | g_rmsf -f $DATA/{}_final -s ${iso}_${ratio}_nosol.tpr -o $output_dir/${iso}_${ratio}_{}_rmsf.xvg -fit -res -ox $output_dir/{}_final -noxvgr $TEST" 
+    
+    clean "${iso}_${ratio}_rmsf"
+}
 
 source ~/.gmx_407
-
-#mode='production'
 
 mode='production'
 
@@ -165,12 +159,7 @@ else
 	TEST="-b 1000 -e 1010"
 fi
 
-
-#ANALYSIS=nonpolar
-#ISO=chiro
-#RATIO=15
-
-trap 'echo "last process ended with retcode=$?"' TERM INT SIGINT EXIT SIGKILL SIGSTOP SIGTERM
+trap 'rm -rf /dev/shm/*; echo "last process ended with retcode=$?"' TERM INT SIGINT EXIT SIGKILL SIGSTOP SIGTERM
 
 base_dir=`pwd`
 DATA="$SCRATCH/inositol/abeta42/2/$RATIO/${ISO}_nonsolvent"
@@ -178,8 +167,5 @@ SHM="/dev/shm/grace/"
 
 mkdir $SHM
 
-echo `pwd`
+# run the analysis
 ${ANALYSIS} $ISO $RATIO $SHM
-
-#remove everything in memory in case signals aren't trapped properly
-#rm -rf /dev/shm/*
