bb63e8b6775513e8b037a1e1fdda0b8f987d1ccb
Grace Li <grace.li@utoronto.ca>
2013-05-11 00:23:44 -0400

    [#49567389] inos3: Finished the porting of the bash nonpolar analysis into python.  It works on a small test.  Need to decide on file naming, integration with writing to pytables instead of flat files, need to test that the current algorithm works for all naming conventions for all systems.  As extra credit, could extend the app with a better configuration system.

--- abeta_analysis/scripts/analysis.py
+++ abeta_analysis/scripts/analysis.py
@@ -1,12 +1,17 @@
 import os
 import subprocess
 import logging
+import string
 
 logging.basicConfig(filename='analysis.log', level=logging.DEBUG)
 
 # TODO: Implement these using configuration. For now leave them as python global variables for convenience
 
 class Analysis(object):
+    def __init__(self):
+        if not os.path.exists('/dev/shm/grace'):
+            os.mkdir('/dev/shm/grace')
+
     RATIO = 15
     ISO = "scyllo"
     DATA_BASE_DIR = "{0}/inositol/abeta42/current/{1}/{2}_nonsolvent".format(os.environ['SCRATCH'], RATIO, ISO)
@@ -20,19 +25,20 @@ class Analysis(object):
     logging.debug("OUTPUT_BASE_DIR=%s", OUTPUT_BASE_DIR)
     logging.debug("START=%s, FINISH=%s", START, FINISH)
 
-    def _clean(self, file_tag):
-        shell_commands = "cd /dev/shm/grace;tar cvfz analysis_%(file_tag)s.tgz *;cp analysis_%(file_tag)s.tgz %(BASE_DIR)s;rm -rf /dev/shm/grace" % vars()
 
-        for cmd in shell_commands.split(";"):
+    # This is the equivalent of performing teardown. But it might be better to perform this clean up action
+    # when the analysis object is destroyed
+    def _clean(self, file_tag):
+        shell_command = "cd /dev/shm/grace; tar cvfz analysis_{0}.tgz *;cp analysis_{0}.tgz {1}; rm -rf /dev/shm/grace".format(file_tag, self.OUTPUT_BASE_DIR)
+        logging.debug("Cleaning up ...")
+        for cmd in shell_command.split(";"):
             logging.debug(cmd)
 
         try:
             # Copy files back to base_dir
-            subprocess.check_call("cd /dev/shm/grace", shell=True)
-            subprocess.check_call("tar cvfz analysis_%(file_tag)s.tgz *" % vars(), shell=True)
-            subprocess.check_call("cp analysis_%(file_tag)s.tgz %(BASE_DIR)s" % vars(), shell=True)
+            subprocess.check_call(shell_command, shell=True)
             # Deletes the files in /dev/shm/grace
-            subprocess.check_call("rm -rf /dev/shm/grace", shell=True)
+            # subprocess.check_call("rm -rf /dev/shm/grace", shell=True)
         except subprocess.CalledProcessError as error:
             # If the subprocess call fails log the error
             logging.debug("Command %s finished with retcode=%s and output %s", 
@@ -41,7 +47,12 @@ class Analysis(object):
             print "Command finished with system error errno=", error.errno 
 
 class NonpolarContactAnalysis(Analysis):
-
+    def _command(self, chain_first, chain_last, insgrp_first, insgrp_last, xtc_filename, system_index, output_dir, testing=False):
+        insgroups = ' '.join([str(i) for i in range(insgrp_first, insgrp_last+1)])
+ 
+        shell_command = string.Template("seq $chain_first $chain_last | parallel -j 16 \"echo {} $index_groups | g_inositol_residue_nonpolar_v2 -f $data_dir/$xtc_name -s ${iso}_${ratio}_nosol.tpr -n ab_${ratio}_${iso}_nonpolar_revised.ndx -per_residue_contacts $output_dir/${sys_index}_chain{}_residue_np_contact.dat -per_inositol_contacts $output_dir/${sys_index}_chain{}_inositol_np_contact.dat -per_residue_table $output_dir/${sys_index}_chain{}_table.dat -num_inositols $num_ligands $testing\"").substitute(chain_first=chain_first, chain_last=chain_last, insgrp_first=insgrp_first, insgrp_last=insgrp_last, data_dir=self.DATA_BASE_DIR, xtc_name=xtc_filename, iso=self.ISO, ratio=self.RATIO, output_dir=output_dir, sys_index=system_index, num_ligands=self.RATIO, index_groups=insgroups, testing='-b 0 -e 100')
+        return shell_command
+ 
     def analyze_contact_by_inositol(self, iso, ratio, num_ligands, output_dir, testing=False):
         logging.debug("Analyzing nonpolar contact for each inositol")
 
@@ -51,27 +62,24 @@ class NonpolarContactAnalysis(Analysis):
         chain_last = 4
 
         # Index of the first inositol group
-        insgrp_first = 1
+        insgrp_first = 5
         # Index of the second inositol group
-        insgrp_last = 2
+        insgrp_last = 19
         
-        data_dir = self.DATA_BASE_DIR
-
-        shell_command = "seq %(chain_first)s %(chain_last)s | parallel -j 16 \"seq %(insgrp_first)s %(insgrp_last)s | g_inositol_residue_nonpolar_v2 -f %(data_dir)s/{0} -s %(iso)s_%(ratio)s_nosol.tpr -n ab_%(ratio)s_%(iso)s_nonpolar_revised.ndx -per_residue_contacts %(output_dir)s/{1}_chain{}_residue_np_contact.dat -per_inositol_contacts %(output_dir)s/{1}_chain{}_inositol_np_contact.dat -per_residue_table %(output_dir)s/{1}_chain{}_table.dat -num_inositols %(num_ligands)s\"" % vars()
-
         if testing:
             logging.debug("Running command as a test")
 
         try:
             for sys_index in range(self.START, self.FINISH):
                 xtc = str(sys_index) + "_final.xtc"
-                if os.path.exists(xtc):
-                    shell_command.format(xtc, sys_index)
+                xtc_full_path = os.path.join(self.DATA_BASE_DIR, xtc)
+                if os.path.exists(xtc_full_path):
+                    shell_command = self._command(chain_first, chain_last, insgrp_first, insgrp_last, xtc, sys_index, output_dir, testing=testing)
                     logging.debug("Running: %s ", shell_command)
+                    print "Running", shell_command
                     subprocess.check_call(shell_command, shell=True)
                 else:
                     logging.debug("The file %s does not exist", xtc)
-
         except subprocess.CalledProcessError as error:
             # If the subprocess call fails log the error
             print "Command", error.cmd, "finished with retcode=", error.returncode, "and output", error.output
@@ -101,6 +109,7 @@ class NonpolarContactAnalysis(Analysis):
 if __name__ == "__main__":
     analysis = NonpolarContactAnalysis()
     print "starting nonpolar analysis"
-    analysis.analyze_contact_by_inositol(Analysis.ISO, Analysis.RATIO, Analysis.RATIO, Analysis.OUTPUT_BASE_DIR + "/test", 
-        testing=True)
+    scratch = Analysis.OUTPUT_BASE_DIR + "/test"
+    temp = '/dev/shm/grace'
+    analysis.analyze_contact_by_inositol(Analysis.ISO, Analysis.RATIO, Analysis.RATIO, temp, testing=True)
 
