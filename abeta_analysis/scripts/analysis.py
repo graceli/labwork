e83eef1bb6f7a04fb5ffb109a78b190afafe325f
Grace Li <grace.li@utoronto.ca>
2013-05-11 20:05:52 -0400

    [#49567389] inos3: ported commands for hbond analysis (essentially g_hbond wrappers) by residue and by inositols. Not yet tested

--- abeta_analysis/scripts/analysis.py
+++ abeta_analysis/scripts/analysis.py
@@ -57,7 +57,7 @@ class NonpolarContactAnalysis(Analysis):
 
         return shell_command
  
-    def analyze_contact_by_inositol(self, iso, ratio, num_ligands, output_dir, testing=False):
+    def analyze_contact_by_ligand(self, iso, ratio, num_ligands, output_dir, testing=False):
         logging.debug("Analyzing nonpolar contact for each inositol")
 
         # Index of the first protein chain
@@ -90,25 +90,140 @@ class NonpolarContactAnalysis(Analysis):
         except OSError as error:
             print "Command finished with system error errno=", error.errno
 
-        self._clean("hbonds_by_inositol")
+        self._clean("nonpolar_contacts_%(iso)s" % vars())
 
-#   function nonpolar_revised {
+
+class HBondContactAnalysis(Analysis):
+    def _command_by_ligand(self, xtc_filename, system_index, output_dir, testing=False):
+        test_command = ''
+        if testing:
+            test_command = '-b 0 -e 100'
+ 
+        shell_command = string.Template("seq 1 ${ratio} | parallel -j 16 \"echo {} Protein | g_hbond -f $data_dir/$xtc_name -s ${iso}_${ratio}_nosol.tpr -n g_hbond_inositol_${iso}_${ratio}.ndx -nonitacc -nomerge -num $output_dir/ab_${iso}_${ratio}_${sys_index}_ins{} -noxvgr $testing > /dev/null 2>&1\"").substitute(data_dir=self.DATA_BASE_DIR, xtc_name=xtc_filename, iso=self.ISO, ratio=self.RATIO, output_dir=output_dir, sys_index=system_index, testing=test_command)
+
+        return shell_command
+
+    def _command_by_residue(self, xtc_filename, system_index, output_dir, testing=False):
+        res_start=0
+        res_end=129
+        ligand_group=130
+
+        test_command = ''
+        if testing:
+            test_command = '-b 0 -e 100'
+ 
+        shell_command = string.Template("seq $res_start $res_end | parallel -j 8 \"echo {} $ligand_grp | g_hbond -f $data_dir/$xtc_name -s ${iso}_${ratio}_nosol.tpr -n g_hbond_${ratio}_${iso}.ndx -nonitacc -nomerge -num $output_dir/ab_${iso}_${ratio}_${sys_index}_residue{} -noxvgr $testing > /dev/null 2>&1\"").substitute(res_start=res_start, res_end=res_end, ligand_grp=ligand_grp, data_dir=self.DATA_BASE_DIR, xtc_name=xtc_filename, iso=self.ISO, ratio=self.RATIO, output_dir=output_dir, sys_index=system_index, testing=test_command)
+
+        return shell_command
+
+    # Calculates the number of hydrogen bonds made with each residue.
+    # g_bond is ran for each ligand molecule
+    def analyze_contact_by_ligand(self, iso, ratio, num_ligands, output_dir, testing=False):
+        logging.debug("Analyzing nonpolar contact for each inositol")
+        
+        if testing:
+            logging.debug("Running command as a test")
+
+        try:
+            for sys_index in range(self.START, self.FINISH):
+                xtc = str(sys_index) + "_final.xtc"
+                xtc_full_path = os.path.join(self.DATA_BASE_DIR, xtc)
+                
+                if os.path.exists(xtc_full_path):
+                    shell_command = self._command(xtc, sys_index, output_dir, testing=testing)
+
+                    logging.debug("Running: %s ", shell_command)
+                    print "Running", shell_command
+                    
+                    subprocess.check_call(shell_command, shell=True)
+                else:
+                    logging.debug("The file %s does not exist", xtc)
+        except subprocess.CalledProcessError as error:
+            # If the subprocess call fails log the error
+            print "Command", error.cmd, "finished with retcode=", error.returncode, "and output", error.output
+        except OSError as error:
+            print "Command finished with system error errno=", error.errno
+
+        self._clean("hbonds_%(iso)s" % vars())
+
+    # Calculates the number of hydrogen bonds made with each residue.
+    # g_hbond is ran each time for each residue-ligands in the protein
+    def analyze_contact_by_residue(self, iso, ratio, num_ligands, output_dir, testing=False):
+        logging.debug("Analyzing nonpolar contact for each inositol")
+        
+        if testing:
+            logging.debug("Running command as a test")
+
+        try:
+            for sys_index in range(self.START, self.FINISH):
+                xtc = str(sys_index) + "_final.xtc"
+                xtc_full_path = os.path.join(self.DATA_BASE_DIR, xtc)
+                
+                if os.path.exists(xtc_full_path):
+                    shell_command = self._command_by_residue(xtc, sys_index, output_dir, testing=testing)
+
+                    logging.debug("Running: %s ", shell_command)
+                    print "Running", shell_command
+                    
+                    subprocess.check_call(shell_command, shell=True)
+                else:
+                    logging.debug("The file %s does not exist", xtc)
+        except subprocess.CalledProcessError as error:
+            # If the subprocess call fails log the error
+            print "Command", error.cmd, "finished with retcode=", error.returncode, "and output", error.output
+        except OSError as error:
+            print "Command finished with system error errno=", error.errno
+
+        self._clean("hbonds_%(iso)s_by_residue" % vars())
+
+
+# # This is a bit of a hack to get g_hbond to work.
+# res_start=0
+# res_end=129
+# INS_grp=130
+# num=0
+# function hbonds {
 #     iso=$1
 #     ratio=$2
-#     output_dir=$3/nonpolar_revised
+#     output_dir=$3/hbonds
 #     mkdir -p $output_dir
-
-#     # We don't need make the index over and over again for the peptides, because they are all the same
-#      #echo -e "'SideChain'&aC*&!rACE\nsplitch16\nq" | make_ndx -f ${iso}_${ratio}_nosol.tpr -o ab_${ratio}_nonpolar.ndx
-
+  
 #     for s in `seq 0 9`; do
-#         xtc="${s}_final"
-#         if [ -e "$DATA/${xtc}.xtc" ]; then
-#             seq $chain1 $chain5 | parallel -j 5 "seq $insgrp_first $insgrp_second | g_inositol_residue_nonpolar_v2 -f $DATA/$xtc -s ${iso}_${ratio}_nosol.tpr -n ab_${ratio}_${iso}_nonpolar_revised.ndx -per_residue_contacts $output_dir/${s}_chain{}_residue_np_contact.dat -per_inositol_contacts $output_dir/${s}_chain{}_inositol_np_contact.dat -per_residue_table $output_dir/${s}_chain{}_table.dat -num_inositols $num_inos $TEST"
-#         fi
+#       xtc="${s}_final"
+#       if [ -e "$DATA/${xtc}.xtc" ]; then
+#           mkdir -p $output_dir/$s
+#           seq $res_start $res_end | parallel -j 8 "echo {} $INS_grp | g_hbond -f $DATA/$xtc -s ${iso}_${ratio}_nosol.tpr -n g_hbond_${ratio}_${iso}.ndx -nonitacc -nomerge -num $output_dir/$s/{} $xvgr $TEST > /dev/null 2>&1"
+#       fi
+#       # python /home/grace/AnalysisScripts/abeta_analysis/abeta_analysis.py sys${s}.h5
 #     done
-#     clean "${iso}_${ratio}_nonpolar"
+
+#     # Cleaning up
+#     clean "${iso}_${ratio}_hbonds"
 # }
+
+
+
+# function hbonds_inositol {
+#     iso=$1
+#     ratio=$2
+#     output_dir=$3/hbonds_inositol
+#     mkdir -p $output_dir
+  
+#     for s in `seq 0 9`; do
+#       xtc="${s}_final"
+#       if [ -e "$DATA/${xtc}.xtc" ]; then
+#           mkdir -p $output_dir/$s
+#           seq 1 $ratio | parallel -j 8 "echo {} Protein | g_hbond -f $DATA/$xtc -s ${iso}_${ratio}_nosol.tpr -n g_hbond_inositol_${iso}_${ratio}.ndx -nonitacc -nomerge -num $output_dir/$s/{} $xvgr $TEST > /dev/null 2>&1"
+#       fi
+#       # python /home/grace/AnalysisScripts/abeta_analysis/abeta_analysis.py sys${s}.h5
+#     done
+
+#     # Cleaning up
+#     clean "${iso}_${ratio}_hbonds_inositol"
+# }
+
+
+
 
 if __name__ == "__main__":
     analysis = NonpolarContactAnalysis()
