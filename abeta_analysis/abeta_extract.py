aee7c64c361ccb113bab46a169b742ea694e4460
Grace Li <grace.li@utoronto.ca>
2013-05-13 13:13:44 -0400

    [#49567389,#49566363] Renamed the script to run the abeta analysis

--- abeta_analysis/abeta_extract.py
+++ abeta_analysis/abeta_extract.py
@@ -3,33 +3,36 @@ import subprocess
 import logging
 import string
 
-logging.basicConfig(filename='analysis.log', level=logging.DEBUG)
+logging.basicConfig(filename=os.path.join(os.environ['PWD'], 'analysis.log'), level=logging.DEBUG)
 
 # TODO: Implement these using configuration. For now leave them as python global variables for convenience
 
 class Analysis(object):
-    def __init__(self):
-        if not os.path.exists('/dev/shm/grace'):
-            os.mkdir('/dev/shm/grace')
 
-    RATIO = 15
-    ISO = "scyllo"
-    DATA_BASE_DIR = "{0}/inositol/abeta42/current/{1}/{2}_nonsolvent".format(os.environ['SCRATCH'], RATIO, ISO)
-    OUTPUT_BASE_DIR = os.environ['PWD']
-    START = 0
-    FINISH = 9
+    def __init__(self, iso, ratio):
+        self.iso = iso
+        self.ratio = ratio
+        self.DATA_BASE_DIR = "{0}/inositol/abeta42/current/{1}/{2}_nonsolvent".format(os.environ['SCRATCH'], ratio, iso)
+        self.START = 0
+        self.FINISH = 9
+        self.TEMP_DIR = '/dev/shm/grace'
 
-    logging.debug("RATIO=%s", RATIO)
-    logging.debug("ISO=%s", ISO)
-    logging.debug("DATA_BASE_DIR=%s", DATA_BASE_DIR)
-    logging.debug("OUTPUT_BASE_DIR=%s", OUTPUT_BASE_DIR)
-    logging.debug("START=%s, FINISH=%s", START, FINISH)
+        logging.debug("RATIO=%s", ratio)
+        logging.debug("ISO=%s", iso)
+        logging.debug("DATA_BASE_DIR=%s", self.DATA_BASE_DIR)
+        logging.debug("START=%s, FINISH=%s", self.START, self.FINISH)
+        logging.debug("Initializing temp dir %s", self.TEMP_DIR)
 
+        try:
+            os.mkdir(self.TEMP_DIR)
+        except OSError as error:
+            logging.debug("Directory %s already exists", self.TEMP_DIR)
 
     # This is the equivalent of performing teardown. But it might be better to perform this clean up action
     # when the analysis object is destroyed
-    def _clean(self, file_tag):
-        shell_command = "cd /dev/shm/grace; tar cvfz analysis_{0}.tgz * --remove-files; cp analysis_{0}.tgz {1}; rm /dev/shm/grace/analysis_{0}.tgz".format(file_tag, self.OUTPUT_BASE_DIR)
+    def _clean(self, file_tag, output_abs_path):
+        shell_command = "cd {2}; tar cvfz analysis_{0}.tgz * --remove-files; cp analysis_{0}.tgz {1}; rm {2}/analysis_{0}.tgz".format(file_tag, output_abs_path, self.TEMP_DIR)
+
         logging.debug("Cleaning up ...")
         for cmd in shell_command.split(";"):
             logging.debug(cmd)
@@ -41,23 +44,30 @@ class Analysis(object):
             # subprocess.check_call("rm -rf /dev/shm/grace", shell=True)
         except subprocess.CalledProcessError as error:
             # If the subprocess call fails log the error
-            logging.debug("Command %s finished with retcode=%s and output %s", 
-                         error.cmd, error.returncode, error.output)
+            logging.debug("Command %s finished with retcode=%s and output %s", error.cmd, error.returncode, error.output)
         except OSError as error:
             print "Command finished with system error errno=", error.errno 
 
 class NonpolarContactAnalysis(Analysis):
-    def _command(self, chain_first, chain_last, insgrp_first, insgrp_last, xtc_filename, system_index, output_dir, testing=False):
+    def __init__(self, iso, ratio, abs_path):
+        super(NonpolarContactAnalysis, self).__init__(iso, ratio)
+        self.output_abs_path = abs_path
+        logging.debug("Analysis results will be outputted to %s", self.output_abs_path) 
+        if not os.path.exists(self.output_abs_path):
+            logging.debug("%s does not exist. Creating ...", self.output_abs_path)
+            os.makedirs(self.output_abs_path)
+
+    def _command(self, insgrp_first, insgrp_last, xtc_filename, system_index, testing=False):
         insgroups = ' '.join([str(i) for i in range(insgrp_first, insgrp_last+1)])
         test_command = ''
         if testing:
             test_command = '-b 0 -e 1000'
  
-        shell_command = string.Template("seq $chain_first $chain_last | parallel -j 16 \"echo {} $index_groups | g_inositol_residue_nonpolar_v2 -f $data_dir/$xtc_name -s ${iso}_${ratio}_nosol.tpr -n ab_${ratio}_${iso}_nonpolar_revised.ndx -per_residue_contacts $output_dir/ab_${iso}_${ratio}_${sys_index}_chain{}_residue_np_contact.dat -per_inositol_contacts $output_dir/ab_${iso}_${ratio}_${sys_index}_chain{}_inositol_np_contact.dat -per_residue_table $output_dir/ab_${iso}_${ratio}_${sys_index}_chain{}_table.dat -num_inositols $num_ligands $testing\"").substitute(chain_first=chain_first, chain_last=chain_last, insgrp_first=insgrp_first, insgrp_last=insgrp_last, data_dir=self.DATA_BASE_DIR, xtc_name=xtc_filename, iso=self.ISO, ratio=self.RATIO, output_dir=output_dir, sys_index=system_index, num_ligands=self.RATIO, index_groups=insgroups, testing=test_command)
+        shell_command = string.Template("seq 0 ${insgrp_last} | g_inositol_residue_nonpolar_v2 -f $data_dir/$xtc_name -s ${iso}_${ratio}_nosol.tpr -n nonpolar_${iso}_${ratio}.ndx -per_residue_contacts $output_dir/ab_${iso}_${ratio}_${sys_index}_residue_np_contact.dat -per_inositol_contacts $output_dir/ab_${iso}_${ratio}_${sys_index}_inositol_np_contact.dat -per_residue_table $output_dir/ab_${iso}_${ratio}_${sys_index}_table.dat -num_inositols $num_ligands $testing").substitute(insgrp_last=insgrp_last, data_dir=self.DATA_BASE_DIR, xtc_name=xtc_filename, iso=self.iso, ratio=self.ratio, output_dir='/dev/shm/grace', sys_index=system_index, num_ligands=self.ratio, index_groups=insgroups, testing=test_command)
 
         return shell_command
  
-    def analyze_contact_by_ligand(self, iso, ratio, num_ligands, output_dir, testing=False):
+    def analyze_contact_by_ligand(self, testing=False):
         logging.debug("Analyzing nonpolar contact for each inositol")
 
         # Index of the first protein chain
@@ -66,9 +76,9 @@ class NonpolarContactAnalysis(Analysis):
         chain_last = 4
 
         # Index of the first inositol group
-        insgrp_first = 5
+        insgrp_first = 1
         # Index of the second inositol group
-        insgrp_last = 19
+        insgrp_last = 64
         
         if testing:
             logging.debug("Running command as a test")
@@ -77,8 +87,9 @@ class NonpolarContactAnalysis(Analysis):
             for sys_index in range(self.START, self.FINISH):
                 xtc = str(sys_index) + "_final.xtc"
                 xtc_full_path = os.path.join(self.DATA_BASE_DIR, xtc)
+                logging.debug("Looking for file %s", xtc_full_path)
                 if os.path.exists(xtc_full_path):
-                    shell_command = self._command(chain_first, chain_last, insgrp_first, insgrp_last, xtc, sys_index, output_dir, testing=testing)
+                    shell_command = self._command(insgrp_first, insgrp_last, xtc, sys_index, testing=testing)
                     logging.debug("Running: %s ", shell_command)
                     print "Running", shell_command
                     subprocess.check_call(shell_command, shell=True)
@@ -90,20 +101,32 @@ class NonpolarContactAnalysis(Analysis):
         except OSError as error:
             print "Command finished with system error errno=", error.errno
 
-        self._clean("nonpolar_contacts_%(iso)s" % vars())
+        self._clean("nonpolar_contacts_{0}_{1}".format(self.iso, self.ratio), self.output_abs_path)
 
 
 class HBondContactAnalysis(Analysis):
-    def _command_by_ligand(self, xtc_filename, system_index, output_dir, testing=False):
+    # TODO: This is duplicated code that should be put into the super class ... if I understood how to call super in python.
+    # Read essential python reference and writing idiomatic python ..
+    def __init__(self, iso, ratio, abs_path):
+        super(HBondContactAnalysis, self).__init__(iso, ratio)
+        self.iso = iso
+        self.ratio = ratio
+        self.output_abs_path = abs_path
+        logging.debug("Analysis results will be outputted to %s", self.output_abs_path) 
+        if not os.path.exists(self.output_abs_path):
+            logging.debug("%s does not exist. Creating ...", self.output_abs_path)
+            os.makedirs(self.output_abs_path)
+
+    def _command_by_ligand(self, xtc_filename, system_index, testing=False):
         test_command = ''
         if testing:
             test_command = '-b 0 -e 100'
  
-        shell_command = string.Template("seq 1 ${ratio} | parallel -j 16 \"echo {} Protein | g_hbond -f $data_dir/$xtc_name -s ${iso}_${ratio}_nosol.tpr -n g_hbond_${ratio}_${iso}_by_ligand.ndx -nonitacc -nomerge -num $output_dir/ab_${iso}_${ratio}_${sys_index}_ins{} $testing > /dev/null 2>&1\"").substitute(data_dir=self.DATA_BASE_DIR, xtc_name=xtc_filename, iso=self.ISO, ratio=self.RATIO, output_dir=output_dir, sys_index=system_index, testing=test_command)
+        shell_command = string.Template("seq 1 ${ratio} | parallel -j 16 \"echo {} Protein | g_hbond -f $data_dir/$xtc_name -s ${iso}_${ratio}_nosol.tpr -n g_hbond_${ratio}_${iso}_by_ligand.ndx -nonitacc -nomerge -num $output_dir/ab_${iso}_${ratio}_${sys_index}_ins{} $testing > /dev/null 2>&1\"").substitute(data_dir=self.DATA_BASE_DIR, xtc_name=xtc_filename, iso=self.iso, ratio=self.ratio, output_dir='/dev/shm/grace', sys_index=system_index, testing=test_command)
 
         return shell_command
 
-    def _command_by_residue(self, xtc_filename, system_index, output_dir, testing=False):
+    def _command_by_residue(self, xtc_filename, system_index, testing=False):
         res_start=0
         res_end=129
         ligand_grp=130
@@ -112,13 +135,13 @@ class HBondContactAnalysis(Analysis):
         if testing:
             test_command = '-b 0 -e 100'
  
-        shell_command = string.Template("seq $res_start $res_end | parallel -j 8 \"echo {} $ligand_grp | g_hbond -f $data_dir/$xtc_name -s ${iso}_${ratio}_nosol.tpr -n g_hbond_${ratio}_by_residue.ndx -nonitacc -nomerge -num $output_dir/ab_${iso}_${ratio}_${sys_index}_residue{} $testing > /dev/null 2>&1\"").substitute(res_start=res_start, res_end=res_end, ligand_grp=ligand_grp, data_dir=self.DATA_BASE_DIR, xtc_name=xtc_filename, iso=self.ISO, ratio=self.RATIO, output_dir=output_dir, sys_index=system_index, testing=test_command)
+        shell_command = string.Template("seq $res_start $res_end | parallel -j 8 \"echo {} $ligand_grp | g_hbond -f $data_dir/$xtc_name -s ${iso}_${ratio}_nosol.tpr -n g_hbond_${ratio}_${iso}_by_residue.ndx -nonitacc -nomerge -num $output_dir/ab_${iso}_${ratio}_${sys_index}_residue{} $testing > /dev/null 2>&1\"").substitute(res_start=res_start, res_end=res_end, ligand_grp=ligand_grp, data_dir=self.DATA_BASE_DIR, xtc_name=xtc_filename, iso=self.iso, ratio=self.ratio, output_dir='/dev/shm/grace', sys_index=system_index, testing=test_command)
 
         return shell_command
 
     # Calculates the number of hydrogen bonds made with each residue.
     # g_bond is ran for each ligand molecule
-    def analyze_contact_by_ligand(self, iso, ratio, num_ligands, output_dir, testing=False):
+    def analyze_contact_by_ligand(self, testing=False):
         logging.debug("Analyzing nonpolar contact for each inositol")
         
         if testing:
@@ -130,7 +153,7 @@ class HBondContactAnalysis(Analysis):
                 xtc_full_path = os.path.join(self.DATA_BASE_DIR, xtc)
                 
                 if os.path.exists(xtc_full_path):
-                    shell_command = self._command_by_ligand(xtc, sys_index, output_dir, testing=testing)
+                    shell_command = self._command_by_ligand(xtc, sys_index, testing=testing)
 
                     logging.debug("Running: %s ", shell_command)
                     print "Running", shell_command
@@ -144,11 +167,11 @@ class HBondContactAnalysis(Analysis):
         except OSError as error:
             print "Command finished with system error errno=", error.errno
 
-        self._clean("hbonds_%(iso)s_by_ligand" % vars())
+        self._clean("hbonds_{0}_{1}_by_ligand".format(self.iso, self.ratio), self.output_abs_path)
 
     # Calculates the number of hydrogen bonds made with each residue.
     # g_hbond is ran each time for each residue-ligands in the protein
-    def analyze_contact_by_residue(self, iso, ratio, num_ligands, output_dir, testing=False):
+    def analyze_contact_by_residue(self, testing=False):
         logging.debug("Analyzing nonpolar contact for each inositol")
         
         if testing:
@@ -160,7 +183,7 @@ class HBondContactAnalysis(Analysis):
                 xtc_full_path = os.path.join(self.DATA_BASE_DIR, xtc)
                 
                 if os.path.exists(xtc_full_path):
-                    shell_command = self._command_by_residue(xtc, sys_index, output_dir, testing=testing)
+                    shell_command = self._command_by_residue(xtc, sys_index, testing=testing)
 
                     logging.debug("Running: %s ", shell_command)
                     print "Running", shell_command
@@ -174,7 +197,7 @@ class HBondContactAnalysis(Analysis):
         except OSError as error:
             print "Command finished with system error errno=", error.errno
 
-        self._clean("hbonds_%(iso)s_by_residue" % vars())
+        self._clean("hbonds_{0}_{1}_by_residue".format(self.iso, self.ratio), self.output_abs_path)
 
 
 # # This is a bit of a hack to get g_hbond to work.
@@ -226,15 +249,12 @@ class HBondContactAnalysis(Analysis):
 
 
 if __name__ == "__main__":
-    hbonds = HBondContactAnalysis()
-    print "starting nonpolar analysis"
-    scratch = Analysis.OUTPUT_BASE_DIR + "/test"
-    temp = '/dev/shm/grace'
-    hbonds.analyze_contact_by_ligand(Analysis.ISO, Analysis.RATIO, Analysis.RATIO, temp, testing=True)
-    hbonds.analyze_contact_by_residue(Analysis.ISO, Analysis.RATIO, Analysis.RATIO, temp, testing=True)
+    OUTPUT_BASE_DIR = os.environ['PWD']
+    hbonds = HBondContactAnalysis(os.path.join(OUTPUT_BASE_DIR, "test/hbonds"))
 
-    nonpolar = NonpolarContactAnalysis()
-    nonpolar.analyze_contact_by_ligand(Analysis.ISO, Analysis.RATIO, Analysis.RATIO, temp, testing=True)
-    
+    hbonds.analyze_contact_by_ligand(Analysis.ISO, Analysis.RATIO, Analysis.RATIO, testing=True)
+    hbonds.analyze_contact_by_residue(Analysis.ISO, Analysis.RATIO, Analysis.RATIO, testing=True)
 
-
+    nonpolar = NonpolarContactAnalysis(os.path.join(OUTPUT_BASE_DIR, "test/nonpolar"))
+    nonpolar.analyze_contact_by_ligand(Analysis.ISO, Analysis.RATIO, Analysis.RATIO, testing=True)
+   
