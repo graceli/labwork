6061565bf4ff60517437945772e0261db3bcd070
Grace Li <grace.li@utoronto.ca>
2013-05-29 23:55:03 -0400

    [finishes #50701745] Inos3: Plots the contact maps and outputs them as pdf images.

--- abeta_analysis/analysis/plot_contact_map.py
+++ abeta_analysis/analysis/plot_contact_map.py
@@ -1,10 +1,11 @@
 import numpy
 import tables
+import pylab
+import os
 
-def plot_contact_map(data, filename):
-	data = numpy.genfromtxt(filename)
+def plot_contact_map(data, data_axis, from_name, to_name):
+	data = numpy.genfromtxt(from_name)
 	pylab.matshow(data)
-	pylab.clim(limits)
 	pylab.colorbar(orientation="horizontal", shrink=1)
 
 	# print "the plot axis labels:", data_axis	
@@ -19,13 +20,13 @@ def plot_contact_map(data, filename):
 	pylab.yticks(y, ylabels)
 
 	#save the figure to disk as a pdf
-	pylab.savefig(filename + '.pdf' % vars())	
-	numpy.savetxt(filename + '.dat', numpy.transpose(data), fmt='%f')
+	pylab.savefig(to_name + '.pdf' % vars())	
+	# numpy.savetxt(to_name + '.dat', numpy.transpose(data), fmt='%f')
 
 
 
-polar_data_axis = [ 'L', 'V', 'F', 'F', 'A', 'E', 'D', 'V', 'G', 'S', 'N', 'K', 'G', 'A', 'I', 'I', 'G', 'L', 'M', 'V', 'G', 'G', 'V', 'V', 'I', 'A' ]
-nonpolar_data_axis = [ 'L', 'V', 'F', 'F', 'A', 'E', 'D', 'V', 'S', 'N', 'K', 'A', 'I', 'I', 'L', 'M', 'V', 'V', 'V', 'I', 'A' ]
+polar_data_axis = [ 'Ace', 'L', 'V', 'F', 'F', 'A', 'E', 'D', 'V', 'G', 'S', 'N', 'K', 'G', 'A', 'I', 'I', 'G', 'L', 'M', 'V', 'G', 'G', 'V', 'V', 'I', 'A' ]
+nonpolar_data_axis = [ 'Ace', 'L', 'V', 'F', 'F', 'A', 'E', 'D', 'V', 'S', 'N', 'K', 'A', 'I', 'I', 'L', 'M', 'V', 'V', 'V', 'I', 'A' ]
 
 for ratio in [15, 64]:
 	for isomer in [ "scyllo", "chiro", "glycerol", "glucose" ]:
@@ -34,6 +35,9 @@ for ratio in [15, 64]:
 
 		nonpolar_name = isomer + "_" + str(ratio) + "_" + "nonpolar_contact_matrix.txt"
 		hbonds_name = isomer + "_" + str(ratio) + "_" + "hbonds_contact_matrix.txt"
-		
-		plot_contact_map(nonpolar_data, nonpolar_name)
-		plot_contact_map(hbonds_data, hbonds_name)
+
+		nonpolar_data = numpy.genfromtxt(nonpolar_name)
+		hbonds_data = numpy.genfromtxt(hbonds_name)
+
+		plot_contact_map(nonpolar_data, nonpolar_data_axis, nonpolar_name, os.path.splitext(nonpolar_name)[0])
+		plot_contact_map(hbonds_data, polar_data_axis, hbonds_name, os.path.splitext(hbonds_name)[0])
